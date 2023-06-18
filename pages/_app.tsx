da113d35551aa3d65b1c740912abf3753c25b61c
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-06-18 10:38:46 +0530

    ChakraUi, Nextjs/Fonts, Customized Theme, Remove default styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// Chakra Provider & Customized Theme
+import { theme } from "../theme"
+import { ChakraProvider } from '@chakra-ui/react'
+
+// Nextjs/Fonts
+import { Lora } from 'next/font/google';
+const lora = Lora({ subsets: ['latin'] });
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+        :root {
+          --font-rubik: ${lora.style.fontFamily};
+        }
+      `}
+      </style>
+
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+
+    </>
+  )
 }
 
 export default MyApp
