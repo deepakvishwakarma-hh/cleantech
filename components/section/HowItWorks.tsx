33e8daaa634f77d9030f56c7f77e4f0395ae2435
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-06-25 14:57:23 +0530

    responsive how-its-works

--- components/section/HowItWorks.tsx
+++ components/section/HowItWorks.tsx
@@ -8,12 +8,12 @@ const HowItWorks = () => {
 
                 <Text fontWeight={500} fontFamily={'heading'} fontSize={'5xl'} textAlign={'center'} >Benefits of Products</Text>
 
-                <Flex gap={[10, 10, 0]} overflowX={['scroll', 'scroll', 'hidden']} justifyContent={'space-between'} py={20} >
+                <Flex gap={[10, 10, 10]} overflowX={['scroll', 'scroll', 'hidden']} justifyContent={'space-between'} py={20} >
                     <Element image="https://images.ctfassets.net/t9x0u6p47op0/787Uv75zGAULU4EokrwKnu/fba2aa544362a197a5f77949ab33b7b9/Find-Your-Routine.png" title="Effectiveness" discription="Take our quiz and tell us about your goals, lifestyle, and values." />
                     <Element image="https://images.ctfassets.net/t9x0u6p47op0/26e4QeSBK8OMAavxaBdui1/5bf4a7b91cfe9fb64ecd32fa093b7491/img.homepage-How2__2_-min.jpg" title="Long Lasting Protection" discription="We’ll personalize a plan backed by science made just for you." />
                     <Element image="https://images.ctfassets.net/t9x0u6p47op0/3wUb7Mz3jh5JdSbSyorHFg/256317b976c1daed9dd0b729a43bc509/Homepage_phone__1_new.jpg" title="Cost Efficient" discription="Build habits that improve your health goals by using our app." />
                 </Flex>
-
+                ``
                 <Divider maxWidth={['100%', '100%', '500px']} margin={'auto'} />
 
                 <Center flexDirection={'column'} py={10}>
@@ -45,9 +45,10 @@ interface Props {
 
 
 const Element: React.FC<Props> = ({ image, title, discription }) => {
+    const responsiveWidth = ['90vw', '372px', '372px']
     return (
-        <Box width={'372px'}>
-            <Box borderRadius={'100%'} width={'372px'} height={'372px'} bgImage={image} bgSize={'cover'}></Box>
+        <Box width={responsiveWidth}>
+            <Box borderRadius={'100%'} width={responsiveWidth} height={responsiveWidth} bgImage={image} bgSize={'cover'}></Box>
             <Text textAlign={'center'} fontSize={'  lg'} letterSpacing={4} mt={25} mb={3}>{title}</Text>
             <Text textAlign={'center'} fontSize={'2xl'} fontFamily={'heading'}>{discription}</Text>
 
