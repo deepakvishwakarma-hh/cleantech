ecfd0f0df3cc628d4117343338b46cddf7ffd38e
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-07-08 16:12:25 +0530

    Update usage.tsx

--- pages/steps/usage.tsx
+++ pages/steps/usage.tsx
@@ -41,7 +41,7 @@ export default function Usage() {
                 <Center pt={'10rem'}>
                     <Flex flexDir={'column'} alignItems={'center'} gap={2} maxW={'1000px'}>
                         <Text px={[2, 2, 1, 0]} fontSize={['2xl', '2xl', '3xl', "4xl"]} fontFamily={'heading'} textAlign={'center'} mb={5} >Select your usage so that we can design your further process accordingly</Text>
-                        <SimpleGrid width={'full'} p={[5, 5, 0]} gap={10} height={['auto', 'auto', '400px']} columns={[1, 1, 3]}>
+                        <SimpleGrid width={'full'} p={[2, 2, 0]} gap={10} height={['auto', 'auto', '400px']} columns={[1, 1, 3]}>
                             {__data.map((data, i) => <Boxes key={i} {...data} selected={data.title === (storage as typeof localstorage['defaultValue']).usage} onclick={() => { handleClick(data.name) }} />)}
                         </SimpleGrid>
                     </Flex>
@@ -55,15 +55,15 @@ export default function Usage() {
 const Boxes = ({ src, alt, title, discription, selected, onclick }: { src: string, alt: string, title: string, discription: string, selected: boolean, onclick: any }) => {
     return (
         <Flex border={selected ? "2px black solid" : "2px white solid"} onClick={onclick} flexDirection={['row', 'row', 'column']} bg="white" rounded={'md'} padding={[2, 2, 10]}>
-            <Center height={['100%', '100%', '50%']}>
+            <Center w={['100px', '100px', 'auto']} height={['100%', '100%', '50%']}>
                 <img
                     alt={alt}
                     src={src}
                     style={{ margin: 'auto' }} />
             </Center>
             <Center alignItems={['start', 'start', 'center']} height={['100%', '100%', '50%']} flexDirection="column" gap={[0, 0, 2]}>
-                <Text textTransform={'capitalize'} fontSize={'xl'} textAlign={'center'} fontFamily={'body'}>{title}</Text>
-                <Text fontSize={'md'} textAlign={['left', 'left', 'center']} fontFamily={'heading'}>{discription}</Text>
+                <Text textTransform={'capitalize'} fontSize={['lg', 'lg', 'xl']} textAlign={'center'} fontFamily={'body'}>{title}</Text>
+                <Text fontSize={['sm', 'sm', 'md']} textAlign={['left', 'left', 'center']} fontFamily={'heading'}>{discription}</Text>
             </Center>
         </Flex>
     )
