b19b079dc8b21fad4f79c4c84759549d2fcac5cd
dag-bag <virenderkumar23435@gmail.com>
2023-08-09 01:29:06 -0700

    Update [slug].tsx

--- pages/recommendation/blogs/[slug].tsx
+++ pages/recommendation/blogs/[slug].tsx
@@ -1,5 +1,37 @@
+import { GetStaticProps } from "next";
 import React from "react";
 
-export default function PostDetails() {
-  return <div></div>;
+export default function PostDetails({ post }: any) {
+  return <div>{JSON.stringify(post)}</div>;
 }
+
+export const getStaticProps: GetStaticProps = async ({ params }: any) => {
+  const { slug } = params;
+  const postResponse = await fetch(
+    `https://customcleansolutions.com/wp-json/wp/v2/posts/${slug}?_embed`
+  );
+  const post = await postResponse.json();
+  return {
+    props: {
+      post,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const posts = await fetch(
+    "https://customcleansolutions.com/wp-json/wp/v2/posts?_fields=id"
+  );
+  const postsData = await posts.json();
+
+  const paths = postsData.map((post: any) => ({
+    params: {
+      id: post.id.toString(),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: true, // false or "blocking"
+  };
+};
