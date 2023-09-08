55e573fbb994c8306e6b47d22f2cf108d9433871
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-09-08 14:41:31 +0530

    product page

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,7 +1,45 @@
 import React from "react";
-
+import {
+  Box,
+  Text,
+  Center,
+  Flex,
+  Container,
+  Stack,
+  Image,
+  useColorModeValue,
+  Heading,
+  NumberIncrementStepperProps,
+} from "@chakra-ui/react";
+import Layout from "~/components/Layout/main";
 export default function Products({ products }: any) {
-  return <div>{JSON.stringify(products)}</div>;
+  return (
+    <Layout>
+      <Container pt={"20vh"} maxW={""} px={"5rem"} gap={10}>
+        <Flex
+          mb={2}
+          alignItems={"center"}
+          borderBottom={"2px gray solid"}
+          justifyContent={"space-between"}
+        >
+          <Text fontSize={"80px"} fontFamily={"heading"}>
+            Shop all
+          </Text>
+          <Text maxW={"sm"} fontSize={"24px"} fontFamily={"heading"}>
+            Full assortment of vitamins and supplements to support all your
+            health needs.
+          </Text>
+        </Flex>
+        <Flex flexWrap={"wrap"} gap={5}></Flex>
+
+        <Flex flexWrap={"wrap"}>
+          {products.map((product: any, index: number) => (
+            <Product key={index} title={product.title.rendered} />
+          ))}
+        </Flex>
+      </Container>
+    </Layout>
+  );
 }
 
 export const getStaticProps = async () => {
@@ -11,3 +49,50 @@ export const getStaticProps = async () => {
   const AllProducts = await res.json();
   return { props: { products: AllProducts } };
 };
+
+const IMAGE =
+  "https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80";
+
+function Product({ title, image, price }: any) {
+  return (
+    <Center py={12}>
+      <Box
+        role={"group"}
+        p={6}
+        maxW={"330px"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.800")}
+        rounded={"lg"}
+        pos={"relative"}
+        zIndex={1}
+      >
+        <Box rounded={"lg"} mt={-12} pos={"relative"} height={"230px"}>
+          <Image
+            rounded={"lg"}
+            height={230}
+            width={282}
+            objectFit={"cover"}
+            src={IMAGE}
+            alt="#"
+          />
+        </Box>
+        <Stack pt={10} align={"center"}>
+          {/* <Text color={"gray.500"} fontSize={"sm"} textTransform={"uppercase"}>
+            Recommended
+          </Text> */}
+          <Heading fontSize={"xl"} fontFamily={"body"} fontWeight={500}>
+            {title}
+          </Heading>
+          <Stack direction={"row"} align={"center"}>
+            <Text fontWeight={800} fontSize={"xl"}>
+              {price}
+            </Text>
+            <Text textDecoration={"line-through"} color={"gray.600"}>
+              $199
+            </Text>
+          </Stack>
+        </Stack>
+      </Box>
+    </Center>
+  );
+}
