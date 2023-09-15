921e0e50531135424cb6447a5aee643712dacb1a
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-09-15 13:50:07 +0530

    remove - react-stars (unusual error)

--- components/atoms/Card/Rating.tsx
+++ components/atoms/Card/Rating.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import ReactStars from "react-stars";
 import { Flex, Text } from "@chakra-ui/react";
 
 interface IRatingProps {
@@ -18,15 +17,6 @@ export const Rating: React.FC<IRatingProps> = ({ rating }) => {
 
   return (
     <Flex align="center">
-      <ReactStars
-        count={5}
-        value={currentRating}
-        half={true}
-        size={24}
-        color2={"#ffd700"}
-        edit={true}
-        onChange={handleRatingChange} // Add onChange to handle rating changes
-      />
       <Text fontSize="xs" mx="1">
         ({rating.count})
       </Text>
