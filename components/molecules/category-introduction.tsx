eb241bdc8ec0932af4f055976c01f53e8233773d
dag-bag <virenderkumar23435@gmail.com>
2023-09-17 15:22:26 +0530

    Update category-introduction.tsx

--- components/molecules/category-introduction.tsx
+++ components/molecules/category-introduction.tsx
@@ -32,22 +32,31 @@ const CategoryIntroduction: React.FC<Props> = ({
           backgroundPosition={"center center"}
           position={"relative"}
         >
-          <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+          <div
+            style={{
+              position: "relative",
+              width: "100%",
+              paddingBottom: "87.5%",
+            }}
+          >
             <Image
               alt=""
               src={`/imgs/${image}`}
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                transition: "opacity 700ms ease-in-out",
+                filter: isLoading
+                  ? "blur(16px) grayscale(100%)"
+                  : "blur(0) grayscale(0)",
+                objectFit: "cover",
+              }}
+              onLoad={() => setLoading(false)}
               fill
-              style={{ objectFit: "cover" }}
-              className={`
-              duration-700 ease-in-out group-hover:opacity-75
-              ${
-                isLoading
-                  ? "scale-110 blur-2xl grayscale"
-                  : "scale-100 blur-0 grayscale-0"
-              })`}
-              onLoadingComplete={() => setLoading(false)}
             />
           </div>
+
           {/* <Image
             style={{ objectFit: "cover" }}
             src={`/imgs/${image}`}
